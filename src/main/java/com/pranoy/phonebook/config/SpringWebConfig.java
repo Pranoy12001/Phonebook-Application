ad502c3e54e35d5a6e49bdcf5e73d2e9bdad239c
Pranoy <pranoy.mbstu12001@gmail.com>
2019-02-23 17:29:32 +0600

    created another class for spring web configure

---
+++ src/main/java/com/pranoy/phonebook/config/SpringWebConfig.java
@@ -0,0 +1,33 @@
+package com.pranoy.phonebook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author Pranoy
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.pranoy"})
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter{
+
+   @Override
+   public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //to do
+   }
+   @Bean 
+   public ViewResolver viewResolver(){
+       InternalResourceViewResolver vr = new InternalResourceViewResolver();
+       vr.setViewClass(JstlView.class);
+       vr.setPrefix("WEB-INF/view");
+       vr.setSuffix(".jsp");
+   } 
+}
